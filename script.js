914de066c1b83a99939c403cc47ffe569ddf8334
DrGloo <raishanbernard@gmail.com>
2017-04-04 21:47:41 +0000

    i love cake

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Script is running");
 var e = document.getElementById("alert11");
-
+var x1 = document.getElementById("thumbnail");
 $("#formGroupExampleInput").change(function() {
     console.log("RUNED");
     $.getJSON(
@@ -9,17 +9,20 @@ $("#formGroupExampleInput").change(function() {
 
 
         function(response) {
-           if(response.Response == "True"){
-            console.log(response);
+            if (response.Response == "True") {
+                console.log(response);
                 console.log("Changing Page");
-                $(".mSearch").html("HI");
-                $(".thumbnail").append("<img src=" + response.Poster + ">");
+                $("#mSearch").html("The " + response.Type + " you've searched for is " + response.Title);
+                $("#mtitle1").html(response.Title);
+                $("#s1").html("hi");
+                $("#desc").html(response.Plot);
+                x1.setAttribute("src", response.Poster);
                 console.log("Changed Page");
-           }
-           else if(response.Response == "False"){
-               console.log("Sorry, we couldn't find it")
-               e.setAttribute("visibility", "visible");
-               $(".alert11").css("visibility", "visible");
-           }
+            }
+            else if (response.Response == "False") {
+                console.log("Sorry, we couldn't find it")
+                e.setAttribute("visibility", "visible");
+                $("#alert11").css("visibility", "visible");
+            }
         });
 });

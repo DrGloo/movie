3080d9f6f221f03a98efd98ef6931dc7fa8512d4
DrGloo <raishanbernard@gmail.com>
2017-05-23 22:02:51 +0000

    hi

--- script.js
+++ script.js
@@ -14,23 +14,26 @@ $("#formGroupExampleInput").change(function() {
             console.log(response[xp])
             for (i = 0; i < response[xp].length; i++) {
                 console.log(response[xp][0])
-                $.getJSON("http://www.omdbapi.com/?i=" + response[xp][0].id,
+                debugger;
+                $.getJSON("http://www.omdbapi.com/?apikey=" + "40e9cece" + "&i=" + response[xp][i].id,
                     function(response) {
                         console.log(response)
                         if (response.Response == "True") {
+                            debugger;
                             console.log(response);
                             console.log("Changing Page");
                             $("#maindiv").append($("#result1").html())
                             console.log($(".mtitle1").length)
                             $("#mSearch").html("The " + response.Type + " you've searched for is " + response.Title);
-                            $(".mtitle1").last().html(response.Title + "(" + response.Year + ")" + '<span class="badge badge-info s1">' +  $(".mtitle1").length  + '</span>');
-                      //      $("#desc").html(response.Plot);
-                        //    $("#rate").html(response.Title);
-                          //  $("#extra").html("Released in " + response.Released + ", " + response.Title +
-                            //" is rated " + response.Rated + ", " + response.Title + " has earrned a total of " +
-                            //response.BoxOffice + ", " + response.Title + " is " + response.DVD + " on " + " DVD")
+                            $("#mtitle1").last().html(response.Title + "(" + response.Year + ")" + '<span class="badge badge-info s1">' +  $(".mtitle1").length
+                            + '</span>');
+                            $("#desc").last().html(response.Plot);
+                            $("#rate").last().html(response.Title);
+                            $("#extra").last().html("Released in " + response.Released + ", " + response.Title +
+                            " is rated " + response.Rated + ", " + response.Title + " has earrned a total of " +
+                            response.BoxOffice + ", " + response.Title + " is " + response.DVD + " on " + " DVD")
                         
-                            x1.setAttribute("src", response.Poster);
+                            $("#thumbnail").last().attr("src", response.Poster);
                             console.log("Changed Page");
                         }
                         else if (response.Response == "False") {
